4593f413dc9b1c01d09930581a3a479ca3cebb30
M Praveen Kumar <87361098+mpraveen075@users.noreply.github.com>
2022-01-27 11:54:00 +0530

    added for loop

--- Hello.java
+++ Hello.java
@@ -1,9 +1,17 @@
 class Hello
 {
+	
  public static void main(String args[])
  {
  	System.out.println("Version 2.0");
 	System.out.println("Hello World");
 	System.out.println("Welcome to Jenkins");
+	 
+	 int a=5;
+	 for(int i=1;i<=10;i++)
+	 {
+		 System.out.println( a +"*" + i +"=" + a*i);
+	 }
+	 
  }
 }
